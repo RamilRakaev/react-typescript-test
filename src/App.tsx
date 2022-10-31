4dbfa7c01333442804741caeea4bd334f29f7d69
Рамиль Ракаев <ramrg546@gmail.com>
2022-10-31 17:51:27 +0300

    Completed Card, added UserItem, UserList. Released getting data from jsonp^Cceholder

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,32 @@
-﻿import React from 'react';
-import Card from './components/Card';
+﻿import axios from 'axios';
+import { useState, useEffect } from 'react';
+import Card, { CardVariant } from './components/Card';
+import UserList from './components/UserList';
+import { IUser } from './types/types';
 
 const App = () => {
+    const [users, setUsers] = useState<IUser[]>([]);
+
+    useEffect(() => {
+        fetchUsers();
+    }, []);
+
+    async function fetchUsers() {
+        try {
+            const response = await axios.get<IUser[]>('https://jsonplaceholder.typicode.com/users');
+            setUsers(response.data);
+        } catch (e) {
+            alert(e);
+        }
+    }
+
     return (
         <div>
-            <Card width='200px' height='200px' />
+            <Card onClick={(num) => console.log(num)} variant={CardVariant.outlined} width='200px' height='200px' >
+                <button>Кнопка</button>
+                <div>ss</div>
+            </Card>
+            <UserList users={users}></UserList>
         </div>
     );
 };
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,16 +1,40 @@
-import React from 'react';
+import React, { FC, useState } from 'react';
+
+export enum CardVariant {
+    outlined = 'outlined',
+    primary = 'primary',
+}
 
 interface CardProps {
     width?: string;
     height?: string;
+    children?: React.ReactChild | React.ReactNode;
+    variant: CardVariant;
+    onClick: (num: number) => void;
 }
 
-const Card = ({ width, height }: CardProps) => {
-    return (
-        <div style={{ width, height, background: 'gray' }}>
+const Card: FC<CardProps> =
+    ({
+        width,
+        height,
+        variant,
+        onClick,
+        children,
+    }) => {
+        const [state, setState] = useState(0);
 
-        </div>
-    );
-};
+        return (
+            <div style={{
+                width,
+                height,
+                border: variant === CardVariant.outlined ? '1px solid gray' : 'none',
+                background: variant == CardVariant.primary ? 'lightgray' : '',
+            }}
+                onClick={() => onClick(state)}
+            >
+                {children}
+            </div>
+        );
+    };
 
 export default Card;
